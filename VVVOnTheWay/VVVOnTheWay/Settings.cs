7caa1fd6b7595e357d8af5f1f7e47cd91cc07d78
Bart Machielsen <bartmachielsen@gmail.com>
2016-12-08 12:55:12 +0100

    added Settings page with values but no Body with comments

---
+++ VVVOnTheWay/VVVOnTheWay/Settings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVVOnTheWay
+{
+   /// <summary>
+   /// Settings for the application
+   /// - Language for the application
+   /// - Progresspath of not finished routes
+   /// - Password for login in application
+   /// </summary>
+    class Settings
+   {
+
+       ///  <value>
+       ///  Default language is set to ENGLISH
+       /// </value>
+       ///  <summary>
+       ///  The preferred language for the application in which the text is shown.
+       ///  <type name="Language"/>
+       ///  </summary>
+       public static Language Language = Language.ENGLISH;
+
+        /// <summary>
+        /// 
+        /// </summary>
+       public static string Password = "";
+        
+        /// <summary>
+        /// The path to saved progress, progress of a Route that has not been finished.
+        /// </summary>
+       public static string ProgressPath = "";
+   }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    enum Language
+    {
+        ENGLISH, DUTCH
+    }
+}
