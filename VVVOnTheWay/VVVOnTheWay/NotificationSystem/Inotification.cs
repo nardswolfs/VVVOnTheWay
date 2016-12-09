f9513e10a6e10187cc6f0268ffc5ff51d1da8dd5
Menno de Jong <MjaJong@users.noreply.github.com>
2016-12-09 13:16:26 +0100

    Notifications (#5)
    * Added the framework for the notifications.
    
    * This should fix all the previous errors
    
    * Added the references

---
+++ VVVOnTheWay/VVVOnTheWay/NotificationSystem/Inotification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVVOnTheWay.NotificationSystem
+{
+    /// <summary>
+    /// The interface every notification implements
+    /// </summary>
+    interface INotification
+    {
+        string Title { get; set; }
+    }
+}
---
+++ VVVOnTheWay/VVVOnTheWay/NotificationSystem/Notification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVVOnTheWay.NotificationSystem
+{
+    /// <summary>
+    /// A general implementation of the INotification interface, having only a text
+    /// </summary>
+    class Notification : INotification
+    {
+        /// <summary>
+        /// Title: The property containing the tile for this notification
+        /// Text: A property that contains thet text for notification, when applicable
+        /// </summary>
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
