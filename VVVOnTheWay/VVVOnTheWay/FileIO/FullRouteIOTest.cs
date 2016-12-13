9d82a6c350a8a4dd76440841156331fe579b170a
Christiaan Paans <christiaanpaans@outlook.com>
2016-12-13 09:33:33 +0100

    unit test json serialize/deserialize (#25)
    * initial setup of FileIO subsystem
    
    * Fixed the FullRouteIO class problem in the solution (not implemented in solution
    
    * deleted duplicate file because of csproj file
    
    * update of FullRouteIO
    re-creation of RouteProgressIO
    
    * everything static, including Bart
    
    * returns Route.Route
    
    * json unit test + several fixes
    
    * added documentation and unnessecary code

---
+++ VVVOnTheWay/VVVOnTheWay/FileIO/FullRouteIOTest.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVVOnTheWay.Route;
+using Windows.Storage;
+
+namespace VVVOnTheWay.FileIO
+{
+    /// <summary>
+    /// Unit Test class for class FullRouteIO
+    /// </summary>
+    class FullRouteIOTest
+    {
+        private JsonSerializerSettings settings = new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.All };
+
+        public FullRouteIOTest()
+        {
+            TestMethod(new RouteTest().PointsOfInterest);
+        }
+
+        public void TestMethod(List<Point> totest)
+        {
+            string s = JsonConvert.SerializeObject(totest, settings);
+            WriteJson(s);
+            ReadJson();
+        }
+
+        public async void WriteJson(string s)
+        {
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            StorageFile sampleFile = await localFolder.CreateFileAsync($"jsontest.json", CreationCollisionOption.ReplaceExisting);
+            await Windows.Storage.FileIO.WriteTextAsync(sampleFile, s);
+        }
+
+        public async void ReadJson()
+        {
+            try
+            {
+                StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+                StorageFile sampleFile = await localFolder.GetFileAsync("jsontest.json");
+                string json = await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
+                List<Point> list = JsonConvert.DeserializeObject<List<Point>>(json, settings);
+            } 
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+        }
+    }
+}
