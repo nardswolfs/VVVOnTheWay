358602675443b9f09aa95734c45895cec937592f
Christiaan Paans <christiaanpaans@outlook.com>
2016-12-19 15:38:24 +0100

    one method for both routes

--- VVVOnTheWay/VVVOnTheWay/FileIO/FullRouteIO.cs
+++ VVVOnTheWay/VVVOnTheWay/FileIO/FullRouteIO.cs
@@ -28,6 +28,28 @@ namespace VVVOnTheWay.FileIO
         /// Loads/creates the Historical Kilometer Route, which can be retrieved as this class' attributes
         /// </summary>
         /// <returns>The Route object for the Historical Kilometer route</returns>
+        public static async Task<Route.Route> LoadRouteAsync(string routeFileName)
+        {
+            StorageFolder datafolder = ApplicationData.Current.LocalFolder;
+            StorageFile routeFile;
+            try
+            {
+                routeFile = await datafolder.GetFileAsync($"{routeFileName}.json");
+            }
+            catch (FileNotFoundException)
+            {
+                return await loadRouteFromAssets(routeFileName);
+            }
+            string json = await Windows.Storage.FileIO.ReadTextAsync(routeFile);
+            if (json == "") return await loadRouteFromAssets(routeFileName);
+            Route.Route retrievedRoute = JsonConvert.DeserializeObject<Route.Route>(json, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+            return retrievedRoute;
+        }
+
+        /// <summary>
+        /// Loads/creates the Historical Kilometer Route, which can be retrieved as this class' attributes
+        /// </summary>
+        /// <returns>The Route object for the Historical Kilometer route</returns>
         public static async Task<Route.Route> LoadHistoricalKilometerRoute()
         {
             StorageFolder datafolder = ApplicationData.Current.LocalFolder;
@@ -68,7 +90,7 @@ namespace VVVOnTheWay.FileIO
             return retrievedRoute;
         }
 
-        private static async Task<Route.Route> loadRouteFromAssets(string routeFileName)
+        private static async Task<Route.Route> loadRouteFromAssetsAsync(string routeFileName)
         {
             StorageFolder datafolder = ApplicationData.Current.LocalFolder;
             StorageFile routeFile = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync($@"Assets\{routeFileName}.json");
