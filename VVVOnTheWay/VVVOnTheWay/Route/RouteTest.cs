155b4c6a9351e3c03ae4cae709bea9b64e0a062a
Bart Machielsen <bartmachielsen@gmail.com>
2016-12-21 19:46:46 +0100

    added some descriptions

--- VVVOnTheWay/VVVOnTheWay/Route/RouteTest.cs
+++ VVVOnTheWay/VVVOnTheWay/Route/RouteTest.cs
@@ -76,48 +76,82 @@ namespace VVVOnTheWay.Route
             //TODO: Bart
             PointsOfInterest.Add(
                 new Point(new Geopoint(new BasicGeoposition {Latitude = 51.5887783, Longitude = 4.7748883})));
-            PointsOfInterest.Add(new PointOfInterest(new[] {"Het Poortje", "Het Poortje"}, new[] {"", ""},
+            PointsOfInterest.Add(new PointOfInterest(new[] { "The Red Gate", "Het Rode Poortje" }, 
+                new[]
+                {
+                    "On the left you see a red gate. At the stone gate are sheep heads and see the coat of arms of Breda. Behind the doors of the gate is a lane that leads to the back of the Grand Place. Earlier this gate served as the deterioration of the meat market which was located on the Grand Place. The gate was designed in 1614 by stonemason Melchior of Herbach. Until 1995 the Breda Museum was confirmed at this property.",
+                     "Hier links zie je een rood poortje. Op de stenen poort zijn schapenkoppen en het wapen van Breda te zien. Achter de deuren van het poortje ligt een steeg die naar de achterkant van de Grote Markt leidt. Vroeger diende dit poortje als achteruitgang van de Vleeshal die zich op de Grote Markt bevond. Het poortje is in 1614 gemaakt door steenhouwer Melchior van Herbach. Tot 1995 was het Breda's Museum in dit pand bevestigd. "
+                },
                 false,
-                new[] {"null", "null"}, null,
+                null, "http://upload.wikimedia.org/wikipedia/commons/4/42/RM10234_Breda_-_Karrestraat_bij_7.jpg",
                 new Geopoint(new BasicGeoposition {Latitude = 51.5881950, Longitude = 4.7751383})));
-            PointsOfInterest.Add(new PointOfInterest(new[] {"Ridderstraat", "Ridderstraat"}, new[] {"", ""},
+
+            PointsOfInterest.Add(new PointOfInterest(new[] {"Ridderstraat", "Ridderstraat"}, 
+                new[] {  "Ridderstraat mall entrance near the Baroness in Breda Centre", "Ridderstraat nabij ingang winkelcentrum de Barones in Breda Centrum" },
                 false,
-                new[] {"null", "null"}, null,
+                null, "https://upload.wikimedia.org/wikipedia/commons/e/eb/Ridderstraat_Breda_Centrum_DSCF2871.JPG",
                 new Geopoint(new BasicGeoposition {Latitude = 51.5870833, Longitude = 4.7757500})));
-            PointsOfInterest.Add(new PointOfInterest(new[] {"Grote Markt", "Grote Markt"}, new[] {"", ""},
+
+            PointsOfInterest.Add(new PointOfInterest(new[] {"Large Market", "Grote Markt" },
+                 new[]
+                 {
+                     "The nightlife mainly takes place in the city of Breda. Also Breda Breda City Centre or called. There are many trade-located around the Havermarkt and the Grote Markt in Breda.",
+                     "Het uitgaansleven speelt zich voornamelijk af in de binnenstad van Breda. Ook wel Breda Centrum of Breda City genaamd. Er zijn vele horeca-gelegen rond de Havermarkt en de Grote Markt in Breda."
+
+                 },
                 false,
-                new[] {"null", "null"}, null,
+                null, "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Grote_Markt_Breda_DSCF2843.JPG/800px-Grote_Markt_Breda_DSCF2843.JPG",
                 new Geopoint(new BasicGeoposition {Latitude = 51.5874167, Longitude = 4.7765550})));
-            PointsOfInterest.Add(new PointOfInterest(new[] {"Bevrijdingsmonument", "Bevrijdingsmonument"},
-                new[] {"", ""},
+
+            PointsOfInterest.Add(new PointOfInterest(new[] { "liberation Monument", "Bevrijdingsmonument" },
+                new[]
+                {
+                    "The monument stands at the intersection of the avenue claudius prince and Wilhelmina canal. Text on the monument: Built In gratitude for liberation in 1944 maria 1954",
+                    "Het monument staat op de kruising van de claudius prinsenlaan en de wilhelminasingel. Tekst op het monument: Uit dank voor bevrijding 1944 gebouwd in mariajaar 1954"
+                },
                 false,
-                new[] {"null", "null"}, null,
+                null, "https://nl.tracesofwar.com/upload/1378100430130014.jpg",
                 new Geopoint(new BasicGeoposition {Latitude = 51.5880283, Longitude = 4.7763333})));
-            PointsOfInterest.Add(new PointOfInterest(new[] {"Stadshuis", "Stadshuis"}, new[] {"", ""},
+
+            PointsOfInterest.Add(new PointOfInterest(new[] { "Breda city hall", "Stadshuis" }, 
+                new[] {"The Breda City Hall on the Grote Markt in the center of Breda. This historic building has a stairway at the front. Find weddings of the Breda public place and official receptions of the mayor and aldermen of Breda. The town hall has a stone landing stage on the railing, lions bearing the arms of Breda and Brabant.",
+                    "Het stadhuis van Breda staat op de Grote Markt in het centrum van Breda. Dit historische gebouw heeft een trap aan de voorkant. Hier vinden trouwerijen van de Bredase burgers plaats en ook officiële ontvangsten van het College van burgemeester en wethouders van Breda. Het stadhuis heeft een stenen bordestrap met op de balustrade, leeuwen die de wapens van Breda en Brabant dragen. "
+                },
                 false,
-                new[] {"null", "null"}, null,
+                null, "https://www.breda.nl/data/files/imagecache/eigen_keuze/450px-p1030219oude_stadhuis_breda.jpg",
                 new Geopoint(new BasicGeoposition {Latitude = 51.5887500, Longitude = 4.7761117})));
+
             PointsOfInterest.Add(
                 new Point(new Geopoint(new BasicGeoposition {Latitude = 51.5879717, Longitude = 4.7763617})));
             PointsOfInterest.Add(
                 new Point(new Geopoint(new BasicGeoposition {Latitude = 51.5875, Longitude = 4.776555})));
-            PointsOfInterest.Add(new PointOfInterest(new[] {"Antonius van Paduakerk", "Antonius van Paduakerk"},
-                new[] {"", ""},
+
+            PointsOfInterest.Add(new PointOfInterest(new[] { "Anthony of Padua Church", "Antonius van Paduakerk" },
+                new[]
+                {
+                    "St. Anthony Cathedral (also called St. Anthony Church) is the cathedral church of the diocese of Breda in St. Jansstraat in Breda. The cathedral is dedicated to St. Anthony of Padua. The cathedral is located in the center of Breda, near the Grote Markt.",
+                    "De Sint-Antoniuskathedraal (ook Sint-Antoniuskerk genoemd) is de kathedrale kerk van het bisdom Breda in de Sint-Jansstraat in Breda. De kathedraal is gewijd aan de heilige Antonius van Padua. De kathedraal bevindt zich in het centrum van Breda niet ver van de Grote Markt.\r\n"
+                },
                 false,
-                new[] {"null", "null"}, null,
+                null, "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/P1000965copySint-Antoniuskathedraal.jpg/266px-P1000965copySint-Antoniuskathedraal.jpg",
                 new Geopoint(new BasicGeoposition {Latitude = 51.5876383, Longitude = 4.77725})));
+
             PointsOfInterest.Add(
                 new Point(new Geopoint(new BasicGeoposition {Latitude = 51.5882783, Longitude = 4.7785})));
+
             PointsOfInterest.Add(new PointOfInterest(new[] {"Library", "Bibliotheek"}, new[] {"", ""},
                 false,
                 new[] {"null", "null"}, null,
                 new Geopoint(new BasicGeoposition {Latitude = 51.588, Longitude = 4.778945})));
+
             PointsOfInterest.Add(
                 new Point(new Geopoint(new BasicGeoposition {Latitude = 51.5873617, Longitude = 4.7802217})));
+
             PointsOfInterest.Add(new PointOfInterest(new[] {"Kloosterkazerne", "Kloosterkazerne"}, new[] {"", ""},
                 false,
                 new[] {"null", "null"}, null,
                 new Geopoint(new BasicGeoposition {Latitude = 51.5877217, Longitude = 4.7810283})));
+
             PointsOfInterest.Add(new PointOfInterest(new[] {"Chasse theater", "Chasse theater"}, new[] {"", ""},
                 false,
                 new[] { "null", "null" }, null,
