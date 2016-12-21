0573ba6122b33bf4ebdd3e6488b41d3fa821bf60
Carnitter <rottor467@gmail.com>
2016-12-21 10:24:17 +0100

    delete settext obsolite

--- VVVOnTheWay/VVVOnTheWay/Pages/MapPage.xaml.cs
+++ VVVOnTheWay/VVVOnTheWay/Pages/MapPage.xaml.cs
@@ -44,7 +44,7 @@ namespace VVVOnTheWay
         public MapPage()
         {
             this.InitializeComponent();
-            SetText();
+            
             Map.MapElementClick += Map_MapElementClick;
             
         }
